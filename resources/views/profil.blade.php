a06c918dfde23754de5521c94fbc46ea20a5f4ad
febrianrizkiadisutiyo <12050116997@students.uin-suska.ac.id>
2023-10-11 23:31:58 +0700

    update table

--- resources/views/profil.blade.php
+++ resources/views/profil.blade.php
@@ -10,11 +10,34 @@
 {{-- @foreach ($profil as $item)
     <h1>{{ $item->alamat }}</h1>
 @endforeach --}}
-@extends('layouts.master')
+{{-- @extends('layouts.master')
 @section('content')
 @foreach ($profil as $item)
     <h1>{{ $item->alamat }}</h1>
 @endforeach
+@endsection --}}
+
+@extends('layouts.master')
+@section('content')
+    {{-- @foreach ($profil as $item)
+        <h1>{{ $item->alamat }}</h1>
+    @endforeach --}}
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th scope="col">id</th>
+                <th scope="col">Nama</th>
+                <th scope="col">Alamat</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($profil as $p)
+            <tr>
+                <td>{{ $p->id }}</td>
+                <td>{{ $p->nama }}</td>
+                <td>{{ $p->alamat }}</td>   
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
 @endsection
-
-
